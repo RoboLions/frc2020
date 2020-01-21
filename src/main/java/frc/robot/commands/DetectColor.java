83dcbac0fbc1cb57c24f71d3c8baf6f782e39f16
Maika <maika.r@prhsrobotics.com>
2020-01-20 19:17:34 -0500

    DetectColor Update

--- src/main/java/frc/robot/commands/DetectColor.java
+++ src/main/java/frc/robot/commands/DetectColor.java
@@ -5,24 +5,43 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
+
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.ColorShim;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.I2C.Port;
 import com.revrobotics.ColorSensorV3;
 import com.revrobotics.ColorMatch;
 import com.revrobotics.ColorMatchResult;
 
 public class DetectColor extends Command {
   
- // public static final ColorSensorV3 colorSensor = new ColorSensorV3(/*put port here*/);
+  private Port i2cport;
+
+public final ColorSensorV3 colorSensor = new ColorSensorV3(i2cport/*put port here*/);
+  // ^^^ this line of code is an error because the port isn't here^ yet
+
+  public int r;
+  public int g;
+  public int b;
+  
+  public void Color(int r, int g, int b) {
+
+  Color red = Color (255, 0, 255);
+  Color yellow = Color (255, 255, 0);
+  Color green = Color (0, 255, 0);
+  Color blue = Color (0, 255, 255);
+
+  }
   
   /*
-  public final Color red = ColorMake (225, 0, 255);
-  public final Color yellow = ColorMake (255, 255, 0);
-  public final Color green = ColorMake (0, 255, 0);
-  public final Color blue = ColorMake (0, 255, 255);
+  public final Color red = ColorMatch.ColorMake (225, 0, 255);
+  public final Color yellow = ColorMatch.ColorMake (255, 255, 0);
+  public final Color green = ColorMatch.ColorMake (0, 255, 0);
+  public final Color blue = ColorMatch.ColorMake (0, 255, 255);
   */
 
   // Called just before this Command runs the first time
@@ -31,9 +50,24 @@ public class DetectColor extends Command {
 
   }
 
+ /*
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    Color detectedColor = colorSensor.getColor();    
+  }
+  */
+
+  @Override
+  public void robotPeriodic() {
+    colorSensor.getRawColor();
+    
+    String colorString;
+    ColorMatchResult match = ColorMatch.matchClosestColor(detectedColor);
+
+    if (match.Color = red) {
+      
+    }
   }
 
   // Make this return true when this Command no longer needs to run execute()
